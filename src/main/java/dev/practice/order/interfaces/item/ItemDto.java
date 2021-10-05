eadc45103ea02102f3d60e2ecce0181529987b5f
ingkellswith <allover3773@gmail.com>
2021-10-05 10:00:00 +0900

    mapstruct를 사용한 객체변환 - item도메인 interface 레이어에 적용

---
+++ src/main/java/dev/practice/order/interfaces/item/ItemDto.java
@@ -0,0 +1,92 @@
+package dev.practice.order.interfaces.item;
+
+import dev.practice.order.domain.item.Item;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+public class ItemDto {
+
+    @Getter
+    @Setter
+    @ToString
+    public static class RegisterItemRequest {
+        private String partnerToken;
+        private String itemName;
+        private Long itemPrice;
+        private List<RegisterItemOptionGroupRequest> itemOptionGroupList;
+        
+        // 기본 매퍼 itemDto -> itemCommand
+        // mapstruct 사용시 주의점
+        // itemDto내의 collection요소, 즉 ItemOptionGroupDto -> ItemOptionGroupCommand 또한 interface에서 명세해줘야 함
+        // 즉 item도메인에서 총 3개의 명세가 필요함
+        // 1. ItemDto.RegisterItemRequest -> ItemCommand.RegisterItemRequest
+        // 2. ItemDto.RegisterItemOptionGroupRequest -> ItemCommand.RegisterItemOptionGroupRequest
+        // 3. ItemDto.RegisterItemOptionRequest -> ItemCommand.RegisterItemOptionRequest
+    }
+
+    @Getter
+    @Setter
+    @ToString
+    public static class RegisterItemOptionGroupRequest {
+        private Integer ordering;
+        private String itemOptionGroupName;
+        private List<RegisterItemOptionRequest> itemOptionList;
+    }
+
+    @Getter
+    @Setter
+    @ToString
+    public static class RegisterItemOptionRequest {
+        private Integer ordering;
+        private String itemOptionName;
+        private Long itemOptionPrice;
+    }
+
+    @Getter
+    @Builder
+    @ToString
+    public static class RegisterResponse {
+        private final String itemToken;
+    }
+
+    @Getter
+    @Setter
+    @ToString
+    public static class ChangeStatusItemRequest {
+        private String itemToken;
+    }
+
+    @Getter
+    @Builder
+    @ToString
+    public static class Main {
+        private final String itemToken;
+        private final Long partnerId;
+        private final String itemName;
+        private final Long itemPrice;
+        private final Item.Status status;
+        private final List<ItemOptionGroupInfo> itemOptionGroupList;
+    }
+
+    @Getter
+    @Builder
+    @ToString
+    public static class ItemOptionGroupInfo {
+        private final Integer ordering;
+        private final String itemOptionGroupName;
+        private final List<ItemOptionInfo> itemOptionList;
+    }
+
+    @Getter
+    @Builder
+    @ToString
+    public static class ItemOptionInfo {
+        private final Integer ordering;
+        private final String itemOptionName;
+        private final Long itemOptionPrice;
+    }
+}
