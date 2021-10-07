42685178ba67b38403c8edca1a69a288a21ad8dc
ingkellswith <allover3773@gmail.com>
2021-10-07 21:44:20 +0900

    Order Domain 구현 및 Order Application 계층 구현

---
+++ src/main/java/dev/practice/order/domain/order/OrderServiceImpl.java
@@ -0,0 +1,47 @@
+package dev.practice.order.domain.order;
+
+import dev.practice.order.domain.order.payment.PaymentProcessor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService {
+    private final OrderStore orderStore;
+    private final OrderReader orderReader;
+    private final OrderItemSeriesFactory orderItemSeriesFactory;
+    private final PaymentProcessor paymentProcessor;
+    private final OrderInfoMapper orderInfoMapper;
+
+    @Override
+    @Transactional
+    public String registerOrder(OrderCommand.RegisterOrder requestOrder) {
+        Order order = orderStore.store(requestOrder.toEntity());
+        orderItemSeriesFactory.store(order, requestOrder);
+        return order.getOrderToken();
+    }
+
+    @Override
+    @Transactional
+    public void paymentOrder(OrderCommand.PaymentRequest paymentRequest) {
+        var orderToken = paymentRequest.getOrderToken();
+        var order = orderReader.getOrder(orderToken);
+        order.orderComplete();
+        // 외부 서비스 호출 시 오류가 발생하지 않으면 그 후에 runtimeexception이 발생해도 외부에서 호출된 로직은 복구되지 않는다.
+        // 따라서 orderComplete이후에 pay를 두어 pay에서 exception이 발생할 경우 롤백되도록 했다.
+        // 이점 : 보상 트랜잭션 로직을 짜지 않아도 됨
+        // 단점 : 로직의 순서를 pay -> orderComplete에서 orderComplete -> pay로 바꾸었으므로 순서를 이렇게 해도 되는건지 주의해야 함
+        paymentProcessor.pay(order, paymentRequest);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public OrderInfo.Main retrieveOrder(String orderToken) {
+        var order = orderReader.getOrder(orderToken);
+        var orderItemList = order.getOrderItemList();
+        return orderInfoMapper.of(order, orderItemList);
+    }
+}
